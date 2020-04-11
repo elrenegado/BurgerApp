ff49c6e674d81e13db0910156836fa7d864102fd
Alan MP <eatcrap_001@hotmail.com>
2020-04-10 19:39:01 -0500

    initial commit, setup finished

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import classes from './App.module.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <header className={classes.Appheader}>
+          <img src={logo} className={classes.Applogo} alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className={classes.Applink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+export default App;
